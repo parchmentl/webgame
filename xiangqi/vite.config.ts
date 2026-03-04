465cdf70fa5cb50e67a8bf8e9ab01903ba500465
parchmentl <cjdslme@126.com>
2026-03-04 15:44:15 +0800

    v1.0.2

--- xiangqi/vite.config.ts
+++ xiangqi/vite.config.ts
@@ -3,6 +3,14 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: './',
   build: {
+    target: 'es2015',
     assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
   },
 });
